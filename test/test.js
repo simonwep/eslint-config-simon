12b92fd13e57531e596761a118ce83a3dcd61222
Simon Reinisch <simon@reinisch.io>
2020-05-19 17:31:20 +0200

    Upgrade eslint and update rules

--- test/test.js
+++ test/test.js
@@ -1,17 +1,21 @@
 const assert = require('assert');
-const eslint = require('eslint');
+const {ESLint} = require('eslint');
 const conf = require('../');
 
 // Extend env rule
 conf.env.node = true;
 
 // Run linter on repo files
-const report = new eslint.CLIEngine(conf).executeOnFiles([
+new ESLint().lintFiles([
     'lib/base.js',
     'lib/node.js',
     'test/test.js'
-]);
+]).then(res => {
+    for (const report of res) {
 
-// Assert no errors and warnings were found.
-assert.strictEqual(report.errorCount, 0);
-assert.strictEqual(report.warningCount, 0);
+        // Assert no errors and warnings were found.
+        assert.strictEqual(report.errorCount, 0);
+        assert.strictEqual(report.warningCount, 0);
+    }
+});
+
